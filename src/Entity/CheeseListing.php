59fb3c1e001ea148de6c8ac3581bb7643c79766f
alcb1310 <97707578+alcb1310@users.noreply.github.com>
2022-04-21 18:57:09 -0500

    formats of data
    Making available different data formats such as jsonhal and cs

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -40,6 +40,13 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
     shortName: 'cheeses',
     attributes:[
         'pagination_items_per_page' => 10,
+        'formats' => [
+            'jsonld',
+            'json',
+            'jsonhal',
+            'html',
+            'csv' =>  ['text/csv']
+        ]
     ]
 )]
 #[ApiFilter(SearchFilter::class, properties: ['title' => 'partial', 'description' => 'partial'])]
